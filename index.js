e057120b49f18840ba7bb1e9ac33d2c4065d8db5
21pa1a0536 <21pa1a0536@vishnu.edu.in>
2025-08-20 13:00:39 +0530

    Changed Js Code to Check Git AND GitHub Functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function sayHello() {
     console.log("Button clicked! This is logged in the console.");
     console.log("Button clicked! This is logged in the console 222.");
     console.log("Hello from feature-hello branch");
+    console.log("Madhu Did a Change to check GIT and GitHub");
+    
 }
 
 // Some example utility functions
